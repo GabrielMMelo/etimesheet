3a44c771c1f7a250ce6d8a7ea25cf0f1f8897b67
brunoqs <brun_o13@hotmail.com>
2018-08-21 22:08:48 -0300

    #CREATE modal form

--- core/views.py
+++ core/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from .models import Timesheet
 # Create your views here.
@@ -6,10 +6,27 @@ from .models import Timesheet
 def index(request):
 	if request.method == "GET":
 		context = {
-			'timesheet': Timesheet.objects.all()
+			'timesheet': Timesheet.objects.all(),
+			
+			#all names without duplicate value
+			'names': Timesheet.objects.all().distinct('name')
 		}
 	else:
 		context = {
 
 		}
 	return render(request, 'core/index.html', context=context)
+
+def save(request):
+	# try using form or model form
+	if request.method == "POST":
+		name = request.POST.get('name')
+		time = request.POST.get('time')
+		day = request.POST.get('day')
+		role = request.POST.get('role')
+		row = request.POST.get('row')
+		column = request.POST.get('column')
+		t = Timesheet(name=name, role=role, time=time, day=day, row=row, column=column)
+		t.save()
+
+		return redirect('index')
