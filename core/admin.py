ec972a3ebfe1c7d958a0a0163303337f9a923202
williamabreu <williamsantos_00@hotmail.com>
2018-12-12 10:22:28 -0200

    Removido person_role do django admin

--- core/admin.py
+++ core/admin.py
@@ -3,8 +3,3 @@ from django.contrib import admin
 from core.models import Person
 
 # Register your models here.
-
-class PersonAdmin(admin.ModelAdmin):
-	list_display = ('role', )
-
-admin.site.register(Person, PersonAdmin)
